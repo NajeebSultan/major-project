71d97164dec2026cc6c100d54bcd9d4c069e0a96
NajeebSultan <najeebsultan070@gmail.com>
2025-09-11 00:39:22 +0500

    Implement show route for individual listings and remove obsolete show.ejs view file

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ app.get("/listings" , async (req, res) => {
 app.get("/listings/:id" , async( req , res) => {
   let {id} = req.params;
   const listing = await Listing.findById(id);
-})
+  res.render("listings/show.ejs", { listing });
+
+});
 
 
 // app.get("/testListing" , (req, res) => {
